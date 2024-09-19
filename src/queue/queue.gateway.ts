cf231e8bc74756b01f6e95df86185e34544a29fe
Joao Lopes <joaoclopes14@gmail.com>
2024-09-19 17:57:43 -0300

    misc

--- src/queue/queue.gateway.ts
+++ src/queue/queue.gateway.ts
@@ -1,6 +1,7 @@
 import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { QueueService } from './queue.service';
+import { RedisService } from '../redis/redis.service';
 
 @WebSocketGateway({
   namespace: '/monitoring',
@@ -13,7 +14,19 @@ import { QueueService } from './queue.service';
 export class QueueGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
 
-  constructor(private readonly queueService: QueueService) { }
+  private pubSubClient;
+  private clientUserMap: Map<string, string> = new Map();
+  // Mapeamento de clientes com batchId
+  private clientEventMap: Map<string, string> = new Map();
+  // Mapeamento para intervalos quando cliente está em primeira posição
+  private clientIntervalMap: Map<string, NodeJS.Timeout> = new Map();
+
+  constructor(
+    private readonly queueService: QueueService,
+    private readonly redisService: RedisService
+  ) {
+    this.pubSubClient = this.redisService.getClient();
+  }
 
   handleConnection(client: Socket) {
     console.log(`Client connected: ${client.id}`);
@@ -21,27 +34,96 @@ export class QueueGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   handleDisconnect(client: Socket) {
     console.log(`Client disconnected: ${client.id}`);
+
+    // Verifica se o cliente estava associado a algum batchId
+    const batchId = this.clientEventMap.get(client.id);
+    const userId = this.clientUserMap.get(client.id);
+    if (batchId && userId) {
+      // Desinscreve o cliente do canal Redis associado ao batchId
+      const channel = `queueUpdate:${batchId}`;
+      this.redisService.redisClient.unsubscribe(channel);
+      this.queueService.removeUserFromQueue(userId, batchId)
+      console.log(`Client ${client.id} unsubscribed from Redis channel ${channel}`);
+
+      // Remove o cliente do mapa
+      this.clientEventMap.delete(client.id);
+    }
+
+    // Limpa o intervalo de envio, se houver
+    const interval = this.clientIntervalMap.get(client.id);
+    if (interval) {
+      clearInterval(interval);
+      this.clientIntervalMap.delete(client.id);
+    }
   }
 
   @SubscribeMessage('manageQueue')
-  async handleJoinQueue(client: Socket, { userId, eventId }: { userId: string, eventId: string }): Promise<void> {
+  async handleJoinQueue(client: Socket, { userId, batchId }: { userId: string, batchId: string }): Promise<void> {
     // Adiciona o usuário à fila e inicia o streaming da posição
-    console.time('addUserToQueue');
-    const position = await this.queueService.addUserToQueue(userId, eventId);
-    console.timeEnd('addUserToQueue');
+    const position = await this.queueService.addUserToQueue(userId, batchId);
     client.emit('queuePosition', { userId, position });
-    this.streamUserPosition(userId, eventId, client);
+    this.clientEventMap.set(client.id, batchId);
+    this.clientUserMap.set(client.id, userId);
+    this.streamUserPosition(userId, batchId, client);
   }
 
-  async streamUserPosition(userId: string, eventId: string, client: Socket): Promise<void> {
+  async streamUserPosition(userId: string, batchId: string, client: Socket): Promise<void> {
+    const channel = `queueUpdate:${batchId}`;
+    // Usando o RedisService para subscrever ao canal Redis
+    this.redisService.redisClient.subscribe(channel);
+    let position = await this.queueService.getUserPosition(userId, batchId);
+    client.emit('queuePosition', { userId, position });
+
+    this.redisService.redisClient.on('message', async (channel, message) => {
+      if (channel === `queueUpdate:${batchId}`) {
+        // Quando uma atualização geral ocorrer, obtenha a posição atualizada do usuário
+        const position = await this.queueService.getUserPosition(userId, batchId);
+
+        // Envia a nova posição para o cliente
+        client.emit('queuePosition', { userId, position });
+
+        // Se o usuário atingir a primeira posição, envia updates periódicos
+        if (position === 1) {
+          this.startPeriodicUpdates(client, userId, batchId);
+        } else {
+          // Se o cliente não está mais na primeira posição, cancela o intervalo
+          const interval = this.clientIntervalMap.get(client.id);
+          if (interval) {
+            clearInterval(interval);
+            this.clientIntervalMap.delete(client.id);
+          }
+        }
+
+        // Se o usuário não estiver mais na fila, cancela a subscrição
+        if (position === null) {
+          this.redisService.redisClient.unsubscribe(channel);
+          this.clientEventMap.delete(client.id); // Remove do mapa
+        }
+      }
+    });
+  }
+
+  startPeriodicUpdates(client: Socket, userId: string, batchId: string) {
+    // Se já houver um intervalo rodando, não inicia outro
+    if (this.clientIntervalMap.has(client.id)) {
+      return;
+    }
+
+    // Define um intervalo de X segundos (ex: 30 segundos) para enviar atualizações
     const interval = setInterval(async () => {
-      const position = await this.queueService.getUserPosition(userId, eventId);
+      const position = await this.queueService.getUserPosition(userId, batchId);
+
+      // Envia a nova posição a cada X segundos
       client.emit('queuePosition', { userId, position });
 
-      // Para de enviar se o usuário atingir a primeira posição ou não estiver mais na fila
-      if (position === null) {
+      // Se o usuário sair da primeira posição, para o intervalo
+      if (position !== 1) {
         clearInterval(interval);
+        this.clientIntervalMap.delete(client.id);
       }
-    }, 15000); // Envia a cada 15 segundos
+    }, 10000);
+
+    // Armazena o intervalo no mapa para controle posterior
+    this.clientIntervalMap.set(client.id, interval);
   }
 }
--- src/queue/queue.service.ts
+++ src/queue/queue.service.ts
@@ -5,29 +5,25 @@ import { RedisService } from '../redis/redis.service';
 export class QueueService {
   constructor(private readonly redisService: RedisService) {}
 
-  // Adiciona um usuário à fila no Redis
-  async addUserToQueue(userId: string, queueId: string): Promise<number> {
-    const queueKey = `queue:${queueId}`; 
-    await this.redisService.redisClient.rpush(queueKey, userId); // Adiciona o usuário no final da fila
-    return this.getUserPosition(userId, queueId);
+  async addUserToQueue(userId: string, batchId: string): Promise<number> {
+    const queueKey = `queue:${batchId}`; 
+    await this.redisService.redisClient.rpush(queueKey, userId);
+    return this.getUserPosition(userId, batchId);
   }
 
-  // Obtém a posição do usuário na fila usando LPos
-  async getUserPosition(userId: string, queueId: string): Promise<number | null> {
-    const queueKey = `queue:${queueId}`;
+  async getUserPosition(userId: string, batchId: string): Promise<number | null> {
+    const queueKey = `queue:${batchId}`;
     const position = await this.redisService.redisClient.lpos(queueKey, userId);
-    return position !== null ? position + 1 : null; // LPos retorna 0-based index, converter para 1-based
+    return position !== null ? position + 1 : null;
   }
 
-  // Remove um usuário da fila (opcional)
-  async removeUserFromQueue(userId: string, queueId: string): Promise<void> {
-    const queueKey = `queue:${queueId}`;
+  async removeUserFromQueue(userId: string, batchId: string): Promise<void> {
+    const queueKey = `queue:${batchId}`;
     await this.redisService.redisClient.lrem(queueKey, 0, userId);
   }
 
-  // Opcional: obter toda a fila
-  async getQueue(queueId: string): Promise<string[]> {
-    const queueKey = `queue:${queueId}`;
-    return await this.redisService.redisClient.lrange(queueKey, 0, -1); // Retorna todos os usuários na fila
+  async getQueue(batchId: string): Promise<string[]> {
+    const queueKey = `queue:${batchId}`;
+    return await this.redisService.redisClient.lrange(queueKey, 0, -1);
   }
 }
